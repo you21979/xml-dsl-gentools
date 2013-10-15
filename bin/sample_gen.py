0aa5f738d43681345364363c3f3ca7d2b0914f27
you21979 <you2197901@gmail.com>
2013-10-15 14:30:48 +0900

    modify command option

--- bin/sample_gen.py
+++ bin/sample_gen.py
@@ -7,6 +7,8 @@ from xml.etree.ElementTree import ElementTree
 
 from optparse import OptionParser
 
+PROGRAM_VERSION = "1.0"
+
 class TestItem(BaseItem):
     def __init__(self):
         BaseItem.__init__(self)
@@ -33,7 +35,7 @@ class Test:
         return
 
 def CommandOption():
-    parser = OptionParser()
+    parser = OptionParser(version="%prog " + PROGRAM_VERSION)
     parser.add_option("-c", "--config", dest="config",
         help="read config file(dsl)", metavar="FILE")
     parser.add_option("-t", "--template_dir", dest="template_dir",
@@ -41,24 +43,28 @@ def CommandOption():
     parser.add_option("-o", "--output_dir", dest="output_dir",
         help="write generate directory", metavar="DIRECTORY")
     parser.add_option("-e", "--encoding", dest="encoding",
-        help="output encoding", metavar="ENCODING")
-    parser.add_option("-p", "--prefix", dest="prefix",
+        help="output encoding", metavar="ENCODING", default="utf8")
+    parser.add_option("-p", "--prefix", dest="prefix", default="",
         help="additional prefix filename", metavar="STRING")
-    parser.add_option("-r", "--release", dest="release",
+    parser.add_option("-r", "--release", dest="release", default="false",
         help="release flag", metavar="BOOL")
 
-    return parser.parse_args()
+    return parser
 
 def main():
 
-    (options, args) = CommandOption()
+    parser = CommandOption()
+    (options, args) = parser.parse_args()
 
-    xmlfile = utils.input_filename(options.config)
-    encode = utils.input_default(options.encoding, "utf8")
-    prefix = utils.input_default(options.prefix, "")
-    template_dir = utils.input_dirname(options.template_dir)
-    output_dir = utils.input_dirname(options.output_dir)
-    release = utils.input_bool(options.release)
+    try:
+        xmlfile = utils.input_filename(options.config)
+        encode = utils.input_default(options.encoding, "")
+        prefix = utils.input_default(options.prefix, "")
+        template_dir = utils.input_dirname(options.template_dir)
+        output_dir = utils.input_dirname(options.output_dir)
+        release = utils.input_bool(options.release)
+    except:
+        return parser.print_help()
 
     generator = Test(xmlfile)
 
