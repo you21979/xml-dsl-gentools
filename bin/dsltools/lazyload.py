143ec3d6f92d54dea40a0794e2df0e0cf82c5fb4
you21979 <you2197901@gmail.com>
2013-10-17 18:58:32 +0900

    add

---
+++ bin/dsltools/lazyload.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+import os
+
+def load(name):
+    try:
+        mod = __import__(name)
+    except:
+        mod_list = name.split('.')
+        mod = __import__('.'.join(mod_list[:-1]))
+
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+def scan(dir):
+    ext = ".py"
+    loaded = []
+    for f in filter(lambda f : f[-len(ext):] == ext, os.listdir(dir)):
+        name = f[:-len(ext)]
+        if name == '__init__':
+            continue
+        loaded.append(load(dir + "." + name))
+    return loaded
+        
